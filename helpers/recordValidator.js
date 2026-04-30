2c290bdc683c8cf142f21003c3c7854c0379439c
Hephzibah Ranjan <beulahranjan@gmail.com>
2026-04-30 12:25:59 +0530

    feat: add validation for creating financial records

---
+++ helpers/recordValidator.js
@@ -0,0 +1,36 @@
+import { ENTERTAINMENT,EXPENSE,FOOD,INCOME,INVESTMENT,PROPERTY,SALARY,HEALTH } from "../utils/constants";
+import { BadRequestError } from "../errors/AppError";
+import { check } from 'express validator';
+import { param } from ' express validator';
+
+export const createRecordValidator = [
+     check('amount','Please Enter a valid amount value')
+      .not()
+      .isEmpty()
+      .isNumeric(),
+
+    check('type')
+    .notEmpty()
+    .withMessage("Type of the record is Required.")
+    .custom((value)=> {
+        if(value === INCOME || value === EXPENSE ) return true;
+
+        throw new BadRequestError('Invalid Type');
+    }),
+
+
+    check('category')
+    .notEmpty()
+    .withMessage('Type of the category is Required.')
+    .custom((value)=>{
+        if(value === FOOD || value === ENTERTAINMENT || value=== HEALTH || value=== SALARY || value === INVESTMENT || value===PROPERTY) return true;
+
+        throw BadRequestError('Invalid Type');
+    }),
+
+    check('description', 'Please Enter a  description')
+    .not()
+    .isEmpty(),
+
+
+]
--- routes/userRoute.js
+++ routes/userRoute.js
@@ -4,9 +4,12 @@ import authorize from "../middlewares/authorize.js";
 import validate from "../middlewares/validate.js";
 import { PERMISSIONS }  from "../utils/permissions.js";
 import userController from "../controllers/userController.js";
-import { createUserValidator, deleteUserValidator} from "../helpers/userValidators";
+import { createUserValidator, deleteUserValidator,updateRoleValidator} from "../helpers/userValidators";
+import { updateStatusValidator } from "../helpers/userValidator.js";
 
 const router = express.Router();
 
 router.post("/",verifyToken,authorize(PERMISSIONS.CREATE_USER),createUserValidator, validate,userController.createUser);
 router.delete("/:id",verifyToken,authorize(PERMISSSIONS.DELETE_USER), deleteUserValidator,validate,userController.deleteUser);
+router.patch("/:id/role/:role", verifyToken,authorize(PERMISSION.ASSIGN_ROLE),updateRoleValidator,validate, userController.updateRole);
+router.patch("/:id/status/:status",verifyToken,authorize(PERMISSIONS.CHANGE_USER_STATUS),updateStatusValidator, validate,userController.updateStatus)
